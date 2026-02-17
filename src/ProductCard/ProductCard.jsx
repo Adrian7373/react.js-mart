81fc13a7dd1beb3623da369073a9d969ce7d58cf
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-02-17 21:15:26 +0800

    sdgrf

--- src/ProductCard/ProductCard.jsx
+++ src/ProductCard/ProductCard.jsx
@@ -3,7 +3,9 @@ import styles from "./ProductCard.module.css";
 export default function ProductCard({ product, onAdd }) {
     return (
         <div className={styles.card}>
-            <img className={styles.productImage} src={product.image} alt="product image" />
+            <div className={styles.productImage}>
+                <img src={product.image} alt="product image" />
+            </div>
             <div className={styles.namePrice}>
                 <p className={styles.productTitle}>{product.title}</p>
                 <p className={styles.productPrice}>${product.price}</p>
